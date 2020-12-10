2d83e9fa185793ecf6ef6fcaa532dd6b8a1e76f7
Michael Doty <michaeldoty@outlook.com>
2020-12-09 19:29:44 -0700

    added images and details to meal items screen

---
+++ components/MealItem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+
+const MealItem = props => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={props.onSelectMeal}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground source={{ uri: props.image }} style={styles.bgImage}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberofLines={1}>{props.title}</Text></View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetails }} >
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity.toUpperCase()}</Text>
+            <Text>{props.affordability}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mealRow: {
+    flexDirection: 'row',
+  },
+  mealItem: {
+    height: 200,
+    width: '100%',
+    backgroundColor: '#f5f5f5',
+    overflow: 'hidden'
+  },
+  mealHeader: {
+    height: '85%'
+  },
+  mealDetails: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '15%'
+  },
+  bgImage: {
+    width: '100%',
+    width: '100%',
+    justifyContent: 'flex-end'
+  },
+  titleContainer: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'center'
+  }
+})
+
+export default MealItem;
--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 
+
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
@@ -9,12 +11,22 @@ const CategoryMealScreen = props => {
   const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
   const renderMealItem = itemData => {
-    return (<View><Text>{itemData.item.title}</Text></View>);
+    return <MealItem
+      title={itemData.item.title}
+      image={itemData.item.imageUrl}
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity}
+      affordability={itemData.item.affordability}
+      onSelectMenu={() => { }} />
   }
 
   return (
     <View style={styles.screen}>
-      <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}>
 
 
       </FlatList>
