d5b46056e58aad738e97a6943f13070512febbd9
Michael Doty <michaeldoty@outlook.com>
2020-12-22 15:02:13 -0700

    added filter buttons and toggles

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/colors';
+
+const FilterSwitch = props => {
+  return (<View style={styles.filterContainer}>
+    <Text>{props.label}</Text>
+    <Switch
+      trackColor={{ true: Colors.primaryColor }}
+      thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+      value={props.state}
+      onValueChange={props.onChange} />
+  </View>)
+
+}
+
+
 
 const FiltersScreen = props => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The FiltersScreen Screen!</Text>
+      <Text style={styles.title}>Availabel Filters / Restrictions</Text>
+      <FilterSwitch
+        label={'Gluten Free'}
+        state={isGlutenFree}
+        onChange={newValue => setIsGlutenFree(newValue)} />
+      <FilterSwitch
+        label={'Lactose Free'}
+        state={isLactoseFree}
+        onChange={newValue => setIsLactoseFree(newValue)} />
+      <FilterSwitch
+        label={'Vegan'}
+        state={isVegan}
+        onChange={newValue => setIsVegan(newValue)} />
+      <FilterSwitch
+        label={'Vegitarian'}
+        state={isVegetarian}
+        onChange={newValue => setIsVegetarian(newValue)} />
+
     </View>
   );
 };
@@ -24,15 +61,37 @@ FiltersScreen.navigationOptions = (navData) => {
             navData.navigation.toggleDrawer();
           }}
         />
-      </HeaderButtons>
+      </HeaderButtons>,
+    headerRight: () =>
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Save'
+          iconName='ios-save'
+          onPress={() => {
+            console.log('Saving filters!');
+          }}
+        />
+      </HeaderButtons>,
   };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+    marginVertical: 10
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center'
   }
 });
 
